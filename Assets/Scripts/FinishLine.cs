956fabb936feb9e99ca808df5432fd10bfbe475a
TheFull0 <joester17@gmail.com>
2026-01-04 22:31:29 +0200

    ctrl s scene

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -14,6 +14,8 @@ public class FinishLine : MonoBehaviour
     {
         MoveAgent.PlayerTouchedFinishLine += OnPlayerTouchedFinishLine;
     }
+    
+    
 
     private void OnPlayerTouchedFinishLine(GameObject player)
     {
